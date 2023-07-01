d4b30c0ee7b3a24f6c92ab338d60d23a3e2ddf25
Gabriel Sanchez <124187943+InTheLittleMoon@users.noreply.github.com>
2023-07-01 11:52:42 -0700

    fixed attempts left and reduction counter

--- src/App.js
+++ src/App.js
@@ -14,40 +14,55 @@ function App() {
   const [attempts, setAttempts] = useState(null);
   const [playable, setPlayable] = useState(true);
 
-  //when your word is assigned, you have 60% of its length in tries
-  const getAttemptsLeft = (arr) => {
-    console.log(arr);
-    const sixtyPercent = arr.length * 0.6;
-    let retVal = Math.floor(sixtyPercent);
-    setAttempts(retVal);
-  };
-
   const getWord = async () => {
-    await fetch("https://random-word-api.herokuapp.com/word")
+    let temp = await fetch("https://random-word-api.herokuapp.com/word")
       .then((res) => res.json())
       .then((data) => {
         setCurrentWord(data[0]);
-        getAttemptsLeft(data);
-        console.log(attempts);
       })
       .catch((error) => console.log("Error in fetch call"));
-
-    console.log("Current word: ", currentWord);
   };
 
-  //should start our game off at launch
-  useEffect(() => {
-    getWord();
-  }, []);
+  //should track attempts per word
+  //when your word is assigned, you have 60% of its length in tries
+  const getAttemptsLeft = (arr) => {
+    console.log("Current word", arr);
+    const sixtyPercent = arr.length * 0.6;
+    let retVal = Math.floor(sixtyPercent);
+    setAttempts(retVal);
+    console.log("Attempts: ", retVal);
+  };
 
+  useEffect(() => {
+    if (currentWord !== "") {
+      getAttemptsLeft(currentWord);
+    }
+  }, [currentWord]);
+
+  // should track wrong letters and decrement attempts, switching playable to false at 0
+  useEffect(() => {
+    if (attempts !== null) {
+      if (attempts === 0) {
+        setPlayable(false);
+      } else {
+        setAttempts((prevVal) => {
+          let newVal = prevVal - 1;
+          return newVal;
+        });
+      }
+    }
+  }, [wrongLetters]);
+
+  //should track keyboard inputs for game when window is primarily focused
   useEffect(() => {
     const handlekeyboardinput = (event) => {
       const { key, keyCode } = event;
       if (playable && keyCode >= 65 && keyCode <= 90) {
         const letter = key.toLowerCase();
+        const word = currentWord;
 
         //if letter choice is correct
-        if (currentWord.includes(letter)) {
+        if (word.includes(letter)) {
           //checks if correct letters does not include the 'letter'
           if (!correctLetters.includes(letter)) {
             setCorrectLetters((currentLetters) => [...currentLetters, letter]);
@@ -70,7 +85,12 @@ function App() {
     window.addEventListener("keydown", handlekeyboardinput);
 
     return () => window.removeEventListener("keydown", handlekeyboardinput);
-  }, [playable, correctLetters, wrongLetters]);
+  }, [playable, correctLetters, wrongLetters, currentWord]);
+
+  //should start our game off at launch
+  useEffect(() => {
+    getWord();
+  }, []);
 
   return (
     <div className="main-container">
